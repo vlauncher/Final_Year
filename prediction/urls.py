0e1acad732caae09c97cfb1a4b2146de345b71b7
v2dent <v2dent@gmail.com>
2023-02-07 00:20:52 +0100

    made changes

--- prediction/urls.py
+++ prediction/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from prediction.views import PredictionView,index
 
 urlpatterns = [
-    path('',index,name='home'),
-    path('predict/',PredictionView.as_view(),name='prediction'),
+    path('',index),
+    path('predict/',PredictionView.as_view()),
 ]
--- server/settings.py
+++ server/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     "corsheaders",
     'rest_framework',
 
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -134,6 +136,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "build/static"),
 ]
 STATIC_ROOT = (BASE_DIR/'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
