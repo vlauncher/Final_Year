57c4f3017c88f64a3cc96f7adc201aebe6fc2187
v2dent <v2dent@gmail.com>
2023-02-08 15:51:40 +0100

    added functionality

--- server/settings.py
+++ server/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "./build"),],
+        'DIRS': [os.path.join(BASE_DIR, "build"),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
