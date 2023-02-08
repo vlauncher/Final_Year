a4ffef30a2186b2a5adcafd0049027ea1a161f09
v2dent <v2dent@gmail.com>
2023-02-08 21:34:52 +0100

    added functionality

--- server/settings.py
+++ server/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -70,7 +69,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "client/build"),],
+        'DIRS': [BASE_DIR, "./client/build"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,9 +132,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "client/build/static"),
-]
+STATICFILES_DIRS = [BASE_DIR, "./client/build/static"]
 STATIC_ROOT = (BASE_DIR/'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
